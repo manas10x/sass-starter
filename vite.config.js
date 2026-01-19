d42a80d50d6e77ed84da232a32fa77e1a1f40103
Manas Lahon <manaslahon@proton.me>
2026-01-19 15:50:22 +0530

    Initial setup: vite+sass multipage

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  root: resolve(__dirname, "src"),
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, "src/index.html"),
+        projects: resolve(__dirname, "src/projects/index.html"),
+        project: resolve(__dirname, "src/project/index.html"),
+      },
+    },
+  },
+});
+
